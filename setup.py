3afe2aacd98b243b3416023dbc53e342f57bf289
efortier <eddy.fortier@umontreal.ca>
2023-05-31 10:33:47 -0400

    modified setup.py with the correct license, project name, version and removed the AuditoryData_pipeline URL

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
-setup(name="AuditoryDataPipeline",
-      version="1.0.0",
+setup(name="Adam_auditory_toolbox",
+      version="0.0.1",
       description="Description is unavailable at the moment.",
-      license="MIT",
+      license="Creative Commons Attribution Share Alike 4.0 International",
       author="E. Fortier",
       author_email="eddy.fortier@umontreal.ca",
-      url="https://github.com/eddyfortier/AuditoryData_pipeline",
+      url="",
       install_requires=["numpy", "pandas",
                         "plotly", "matplotlib",
                         "sklearn", "colorama",
