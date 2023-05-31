934f68263010f26cec2952cf3ded3bf8589f71c1
efortier <eddy.fortier@umontreal.ca>
2023-05-31 11:27:45 -0400

    deactivated the graph, report and MRI session functionalities

--- Adam_auditory_toolbox.py
+++ Adam_auditory_toolbox.py
@@ -1,15 +1,15 @@
 import os
 import colorama as color
 
-from src import report_PTA
-from src import report_MTX
-from src import report_DPOAE as report_DP
-from src import report_DPGrowth as report_DPGr
+#from src import report_PTA
+#from src import report_MTX
+#from src import report_DPOAE as report_DP
+#from src import report_DPGrowth as report_DPGr
 
 from src import BIDS_formater as formater
 from src import json_sidecar_generator as jsg
-from src import MRI_session_design_generator as ses_design
-from src import graph_generator_BIDS as graph
+#from src import MRI_session_design_generator as ses_design
+#from src import graph_generator_BIDS as graph
 
 
 # Initialize colorama
@@ -18,16 +18,16 @@ color.init(autoreset=True)
 # Available functions list
 ls_fct = ["BIDS format's json sidecars creation",
           "BIDS format's auditory data exporter",
-          "Pure Tone Audiometry interactive graph generator",
-          "Matrix Speech-in-Noise Test interactive graph generator",
-          "Transient-evoked OAE test graph generator",
-          "Distortion product OAE test graph generator",
-          "Distortion product growth function test graph generator",
-          "Pure Tone Audiometry report generator",
-          "Matrix Speech-in-Noise Test report generator",
-          "Distortion product OAE report generator",
-          "Distortion product growth function report generator",
-          "MRI session design files generator (in development)",
+#          "Pure Tone Audiometry interactive graph generator",
+#          "Matrix Speech-in-Noise Test interactive graph generator",
+#          "Transient-evoked OAE test graph generator",
+#          "Distortion product OAE test graph generator",
+#          "Distortion product growth function test graph generator",
+#          "Pure Tone Audiometry report generator",
+#          "Matrix Speech-in-Noise Test report generator",
+#          "Distortion product OAE report generator",
+#          "Distortion product growth function report generator",
+#          "MRI session design files generator (in development)",
           # "Dummy line",
           ]
 
@@ -52,7 +52,7 @@ loop_value = True
 # Show welcome message
 print(color.Style.BRIGHT
       + color.Fore.YELLOW
-      + "\nWelcome to the AuditoryData_pipeline.\n")
+      + "\nWelcome to the Adam_auditory_toolbox.\n")
 
 # function selection prompt
 while loop_value:
@@ -98,82 +98,84 @@ while loop_value:
                                                 os.path.join(".", "results"))
                             print("\n")
 
-                    # Graph generation functionalities
-                    elif ls_fct[value - 1].count("graph") == 1:
+#                    # Graph generation functionalities
+#                    elif ls_fct[value - 1].count("graph") == 1:
 
-                        # PTA graph plotting
-                        if ls_fct[value - 1].count("Pure Tone") == 1:
-                            graph.master_run(".", "PTA")
-                            print("\n")
+#                        # PTA graph plotting
+#                        if ls_fct[value - 1].count("Pure Tone") == 1:
+#                            graph.master_run(".", "PTA")
+#                            print("\n")
 
-                        # MTX graph plotting
-                        elif ls_fct[value - 1].count("Matrix") == 1:
-                            graph.master_run(".", "MTX")
-                            print("\n")
+#                        # MTX graph plotting
+#                        elif ls_fct[value - 1].count("Matrix") == 1:
+#                            graph.master_run(".", "MTX")
+#                            print("\n")
 
-                        # TEOAE graph plotting
-                        elif ls_fct[value - 1].count("Transient") == 1:
-                            graph.master_run(".", "TEOAE")
-                            print("\n")
+#                        # TEOAE graph plotting
+#                        elif ls_fct[value - 1].count("Transient") == 1:
+#                            graph.master_run(".", "TEOAE")
+#                            print("\n")
 
-                        # Distortion product (DPOAE and DP Growth) OAEs
-                        elif ls_fct[value - 1].count("Distortion"):
+#                        # Distortion product (DPOAE and DP Growth) OAEs
+#                        elif ls_fct[value - 1].count("Distortion"):
 
-                            # DPOAE graph plotting
-                            if ls_fct[value - 1] == ("Distortion product OAE "
-                                                     "test graph generator"):
-                                graph.master_run(".", "DPOAE")
-                                print("\n")
+#                            # DPOAE graph plotting
+#                            if ls_fct[value - 1] == ("Distortion product OAE "
+#                                                     "test graph generator"):
+#                                graph.master_run(".", "DPOAE")
+#                                print("\n")
 
-                            # DP Growth graph plotting
-                            elif ls_fct[value - 1] == ("Distortion product "
-                                                       "growth function test "
-                                                       "graph generator"):
-                                graph.master_run(".", "Growth")
-                                print("\n")
+#                            # DP Growth graph plotting
+#                            elif ls_fct[value - 1] == ("Distortion product "
+#                                                       "growth function test "
+#                                                       "graph generator"):
+#                                graph.master_run(".", "Growth")
+#                                print("\n")
 
-                    # Report generation capabilities
-                    elif ls_fct[value - 1].count("report") == 1:
+#                    # Report generation capabilities
+#                    elif ls_fct[value - 1].count("report") == 1:
 
-                        # PTA report generation
-                        if ls_fct[value - 1].count("Pure Tone") == 1:
-                            report_PTA.master_run(os.path.join(".", "results"))
-                            print("\n")
+#                        # PTA report generation
+#                        if ls_fct[value - 1].count("Pure Tone") == 1:
+#                            report_PTA.master_run(os.path.join(".",
+#                                                               "results"))
+#                            print("\n")
 
-                        # MTX report generation
-                        elif ls_fct[value - 1].count("Matrix") == 1:
-                            report_MTX.master_run(os.path.join(".", "results"))
-                            print("\n")
+#                        # MTX report generation
+#                        elif ls_fct[value - 1].count("Matrix") == 1:
+#                            report_MTX.master_run(os.path.join(".",
+#                                                               "results"))
+#                            print("\n")
 
-                        # Distortion product (DPOAE and DP Growth) OAEs
-                        elif ls_fct[value - 1].count("Distortion"):
+#                        # Distortion product (DPOAE and DP Growth) OAEs
+#                        elif ls_fct[value - 1].count("Distortion"):
 
-                            # DPOAE report generation
-                            if ls_fct[value - 1] == ("Distortion product OAE "
-                                                     "report generator"):
-                                report_DP.master_run(os.path.join(".",
-                                                                  "results"))
-                                print("\n")
+#                            # DPOAE report generation
+#                            if ls_fct[value - 1] == ("Distortion product OAE "
+#                                                     "report generator"):
+#                                report_DP.master_run(os.path.join(".",
+#                                                                  "results"))
+#                                print("\n")
 
-                            # DP Growth report generation
-                            elif ls_fct[value - 1] == ("Distortion product "
-                                                       "growth function "
-                                                       "report generator"):
-                                report_DPGr.master_run(os.path.join(".",
-                                                                    "results"))
-                                print("\n")
+#                            # DP Growth report generation
+#                            elif ls_fct[value - 1] == ("Distortion product "
+#                                                       "growth function "
+#                                                       "report generator"):
+#                                report_DPGr.master_run(os.path.join(".",
+#                                                                    "results"))
+#                                print("\n")
 
-                    # MRI sessions design files generation functionalities
-                    elif ls_fct[value - 1].count("design files") == 1:
-                        ses_design.master_run(os.path.join(".", "data"))
-                        print("\n")
+#                    # MRI sessions design files generation functionalities
+#                    elif ls_fct[value - 1].count("design files") == 1:
+#                        ses_design.master_run(os.path.join(".", "data"))
+#                        print("\n")
 
                     # Test Dummy
                     elif ls_fct[value - 1].count("Dummy") == 1:
                         print("This is just a test line:",
                               ls_fct[value - 1],
                               "\n")
-        
+
     ###########################################################################
 
             else:
@@ -200,4 +202,4 @@ while loop_value:
 # Exit message
 print(color.Style.BRIGHT
       + color.Fore.YELLOW
-      + "Thanks for using the AuditoryData_pipeline.\n")
+      + "Thanks for using the Adam_auditory_toolbox.\n")
